0d8ddabe101ada9e94977ce47464bc63e9f08f7e
lee le <wlsduddl013@gmail.com>
2020-09-29 10:55:53 +0900

    app 조회

--- 10.http모듈/app.js
+++ 10.http모듈/app.js
@@ -3,24 +3,38 @@ const url = require('url');
 const qs = require('querystring');
 const fs = require('fs');
 const view = require('./view/index');
+const template = require('./view/template');
 
-http.createServer(function(req,res) {
+http.createServer(function (req, res) {
     let pathname = url.parse(req.url).pathname;
-    switch(pathname) {
-    case '/':
-        fs.readdir('data', function(error, filelist) {
-            let list = '';
-            for (let file of filelist) {
-                let filename = file.substring(0, file.length-4);
-                list +=  `<li><a href="/?id=${filename}">${filename}</a></li>`;
+    let query = url.parse(req.url, true).query;
+    console.log(pathname, query.id) // 확인용
+    switch (pathname) {
+        case '/': // 초기(홈)화면_초기화면에서는 수정 삭제버튼 작동이 필요없기에 tem.js에서 뺌
+            if (query.id === undefined) {
+                fs.readdir('data', function (error, filelist) {
+                    let list = template.listGen(filelist);
+                    let content = template.HOME_CONTENTS;
+                    let control = template.buttonGen();
+                    let html = view.index('Web 기술', list, content, control); // 웹화면의 탭 부분
+                    res.end(html);
+                });
+            } else { // id(목록)_부분
+                fs.readdir('data', function(error, filelist) {
+                    let list = template.listGen(filelist);
+                    let title = query.id;
+                    let control = template.buttonGen(title);
+                    let filename = 'data/' + title + '.txt';
+                    fs.readFile(filename, 'utf8', (error, buffer) => {
+                        let html = view.index(title, list, buffer, control);
+                        res.end(html);
+                    });
+                });
             }
-            let html = view.index(list);
-            res.end(html);
-        })
-        break;
-    default:
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end();
+            break;
+        default:
+            res.writeHead(404);
+            res.end();
     }
 }).listen(3000, () => {
     console.log('Server Running at http://localhost:3000')
